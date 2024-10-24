459166c0ad29ab697bf59651aa5663b004a76327
tomastoler <tomastoler@gmail.com>
2024-10-24 17:15:07 -0300

    new global styles

--- app/js/navbar.js
+++ app/js/navbar.js
@@ -5,18 +5,22 @@ const $header = $('#header')
 function navbar() {
     const user = JSON.parse(localStorage.getItem('user'))
     if (!user) {
-        $header.innerHTML = `<nav>
-            <ul>
-                <li> <a href="/index.html">Home</a> </li>
-                <li> <a href="/auth/register.html">Registrarse</a> </li>
-                <li> <a href="/auth/login.html">Ingresar</a> </li>
+        $header.innerHTML = `
+        <h1>Disket</h1>
+        <nav>
+            <ul class="flex list-none gap-2">
+                <li> <a class="hover:underline ml-2" href="/index.html">Home</a> </li>
+                <li> <a class="hover:underline ml-2" href="/auth/register.html">Registrarse</a> </li>
+                <li> <a class="hover:underline ml-2" href="/auth/login.html">Ingresar</a> </li>
             </ul>
         </nav>`
         return
     }
 
-    $header.innerHTML = `<nav>
-            <ul>
+    $header.innerHTML = `
+        <h1>Disket</h1>    
+        <nav>
+            <ul class="flex list-none">
                 <button id="logout-button" >Cerrar sesión</button>
             </ul>
         </nav>`
